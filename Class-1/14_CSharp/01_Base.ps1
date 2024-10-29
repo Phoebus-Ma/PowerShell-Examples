54e6be5bc8903c864377a10a471a4c2220a47d92
Phoebus <tianjindahai@126.com>
2024-10-29 16:47:48 +0800

    [Ps1] Add get system temperature example.

--- Class-1/14_CSharp/01_Base.ps1
+++ Class-1/14_CSharp/01_Base.ps1
@@ -47,26 +47,3 @@ Write-Output "----------------Test 3----------------"
 # Add types from an assembly.
 Set-Location -Path $PSHOME
 $AccType = Add-Type -AssemblyName *jsonschema* -PassThru
-
-
-Write-Output "----------------Test 4----------------"
-# Call native Windows APIs.
-$Signature = @"
-[DllImport("user32.dll")]public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
-"@
-
-$addTypeSplat = @{
-    MemberDefinition = $Signature
-    Name = "Win32ShowWindowAsync"
-    Namespace = 'Win32Functions'
-    PassThru = $true
-}
-$ShowWindowAsync = Add-Type @addTypeSplat
-
-# Minimize the PowerShell console
-
-$ShowWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 2)
-
-# Restore the PowerShell console
-
-$ShowWindowAsync::ShowWindowAsync((Get-Process -Id $Pid).MainWindowHandle, 4)
