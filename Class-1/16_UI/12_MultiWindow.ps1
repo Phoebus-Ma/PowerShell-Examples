262fef8d13091d9bc5d1eb351870dad7a97f64ed
Phoebus <tianjindahai@126.com>
2024-11-14 20:35:05 +0800

    [Ps1] Add wpf multi windows in powershell example.

---
+++ Class-1/16_UI/12_MultiWindow.ps1
@@ -0,0 +1,43 @@
+###
+# WPF multi windows in powershell example.
+#
+# License - MIT.
+###
+
+Add-Type -Assembly PresentationFramework
+
+# Convert a .XAML file to an XMLDocument
+function LoadXml ($filename) {
+    $XmlLoader = (New-Object System.Xml.XmlDocument)
+    $XmlLoader.Load($filename)
+
+    return $XmlLoader
+}
+
+# Load the XAML files.
+$XmlMainWindow = LoadXml("$PSScriptRoot\Xaml\MainWindow.xaml")
+$XmlSecWindow  = LoadXml("$PSScriptRoot\Xaml\SecWindow.xaml")
+
+# Collection storing references to all named WPF controls in the UI.
+$UIControls = [hashtable]::Synchronized(@{})
+
+# Convert Windows and Pages to a XAML object graph.
+$UIControls.MainWindow = [Windows.Markup.XamlReader]::Load((New-Object -TypeName System.Xml.XmlNodeReader -ArgumentList $XmlMainWindow))
+$UIControls.SecWindow  = [Windows.Markup.XamlReader]::Load((New-Object -TypeName System.Xml.XmlNodeReader -ArgumentList $XmlSecWindow))
+
+# Add each named control to the $UIControls hashtable (repeat for each Window / Page).
+$XmlMainWindow.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]") | ForEach-Object -Process {
+    $UIControls.$($_.Name) = $UIControls.MainWindow.FindName($_.Name)
+}
+
+$XmlSecWindow.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]") | ForEach-Object -Process {
+    $UIControls.$($_.Name) = $UIControls.SecWindow.FindName($_.Name)
+}
+
+# Button.
+$UIControls.btn_hello.Add_Click({
+    $UIControls.SecWindow.ShowDialog()
+})
+
+# Display main window.
+$UIControls.MainWindow.ShowDialog()
