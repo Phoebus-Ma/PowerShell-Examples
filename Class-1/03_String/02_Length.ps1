aed5687a6db19a229123701fa36d86c37e8f4afc
Phoebus <tianjindahai@126.com>
2024-11-18 20:35:12 +0800

    [Ps1] Add get string length example.

---
+++ Class-1/03_String/02_Length.ps1
@@ -0,0 +1,15 @@
+###
+# String length in powershell example.
+#
+# License - MIT.
+###
+
+Write-Output "----------------Test 1----------------"
+# Result: 11.
+"hello world".Length
+
+
+Write-Output "----------------Test 2----------------"
+# Resutl: 29.
+$str = "hello world, happy every day."
+$str.Length
