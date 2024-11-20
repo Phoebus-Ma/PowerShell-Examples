629a3c8073f7eb1f0e2a0af8f35bae2f5ac61543
Phoebus <tianjindahai@126.com>
2024-11-20 17:35:26 +0800

    [Ps1] Add write json to file.

---
+++ Class-1/07_File/13_WriteJson.ps1
@@ -0,0 +1,43 @@
+###
+# Write json in powershell example.
+#
+# License - MIT.
+###
+
+Write-Output "----------------Test 1----------------"
+# Simple write json to file.
+$jsonPath = ".\res\JsonFile.json"
+
+'{ "key1":"value1", "key2":"value2" }' | Out-File $jsonPath
+$str = Get-Content $jsonPath
+Write-Output ($str)
+
+
+Write-Output "----------------Test 2----------------"
+# Save json file.
+$jsonPath = ".\res\JsonFile2.json"
+
+# Create a PowerShell object.
+$person = @{
+    FirstName = 'James'
+    LastName  = 'Karley'
+    Age = 26
+}
+
+# Convert the object to a JSON string.
+$json = $person | ConvertTo-Json
+
+# Write the JSON string to a file.
+$json | Out-File -FilePath $jsonPath
+
+
+Write-Output "----------------Test 3----------------"
+# Loop save json file.
+$jsonPath = ".\res\JsonFile3.json"
+
+1..5 | ForEach-Object {
+    New-Object PSObject -Property @{
+        Id = $_
+        Num = (Get-Random).ToString()
+    }
+} | ConvertTo-Json | Out-File -FilePath $jsonPath -Append
