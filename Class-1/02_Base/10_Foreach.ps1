036ca1b951bad7febd95629faf41740c9b2aa7e8
Phoebus <tianjindahai@126.com>
2024-11-20 10:59:03 +0800

    [Ps1] Update ForEach-Object method.

--- Class-1/02_Base/10_Foreach.ps1
+++ Class-1/02_Base/10_Foreach.ps1
@@ -5,7 +5,7 @@
 ###
 
 Write-Output "----------------Test 1----------------"
-
+# Print array member.
 $letterArray = 'a','b','c','d'
 foreach ($letter in $letterArray)
 {
@@ -14,11 +14,18 @@ foreach ($letter in $letterArray)
 
 
 Write-Output "----------------Test 2----------------"
-
+# Print process id more than 20000.
 foreach ($Proc in Get-Process)
 {
     if ($Proc.Id -gt 20000)
     {
         Write-Output $Proc
     }
+}
+
+
+Write-Output "----------------Test 3----------------"
+# Print 1~10.
+1..10 | ForEach-Object {
+    Write-Output $_
 }
